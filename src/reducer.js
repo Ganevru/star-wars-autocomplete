f6a9d74bcfa654e95cafff73f92e5e6ebba0c363
Ivan Ganev <iganevru@gmail.com>
2020-01-07 21:53:43 +0300

    fix bug

--- src/reducer.js
+++ src/reducer.js
@@ -3,9 +3,9 @@ import { combineReducers } from 'redux';
 import { FETCH_CHARACTERS_FULFILLED } from './actions';
 
 const charactersReducer = (characters = [], action) => {
-  switch (action.key) {
+  switch (action.type) {
     case FETCH_CHARACTERS_FULFILLED:
-      return action.payload.characters;
+      return action.payload;
     default:
       return characters;
   }
